c1197e0dd29ef4d64bb895f0e91e697ee6d3b4f7
aboli105 <aboswaghmare125@gmail.com>
2022-03-15 17:56:23 +0530

    new

--- home.js
+++ home.js
@@ -4,3 +4,32 @@ function loadCoupn(){
 function closeCoupn(){
     document.getElementById('coupn').style.visibility='hidden'
 }
+var x = document.getElementById("weather")
+var y = document.getElementById("cityname")
+function geolocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition)
+    }else{
+        x.innerText="Geo Not Supported"
+    }
+}
+
+function showPosition(data){
+    console.log(data)
+    let latitude = data.coords.latitude;
+    let longitude = data.coords.longitude
+    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&mode=json&units=metric&cnt=1&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+    
+    fetch(url,{method: 'GET'})
+
+    .then((res) => res.json())
+
+    .then((data) => {
+        let cityName = data.city.name
+        let weather = `${data.list[0].temp.day} C°`
+        console.log(data)
+        x.innerText = weather;
+        y.innerText = cityName;
+        
+    })
+}
